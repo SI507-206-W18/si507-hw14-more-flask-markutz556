c0268d16b7ca83b13abee6dde27d7c06d6c04110
markutz556 <2917097440@qq.com>
2018-04-16 17:55:40 -0400

    add app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask, render_template, request, redirect
 import model
 
@@ -12,6 +13,17 @@ def index():
 def addentry():
     ## add a guestbook entry
     return render_template("addentry.html")
+
+@app.route("/admin")
+def admin():
+    ## print the guestbook
+    return render_template("admin.html", entries=model.get_entries())
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    del_id = request.form['id']
+    model.delete_entry(del_id)
+    return redirect("/admin")
 
 @app.route("/postentry", methods=["POST"])
 def postentry():
